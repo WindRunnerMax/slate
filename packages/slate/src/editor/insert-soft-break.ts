b1db3f23b79e9d7a1cb8c3f5f376bc76a7692c25
Czy <651525974@qq.com>
2024-07-10 11:43:13 +0800

    Fix line break behavior in rich-text example
    Related to #5671
    
    Update `insertSoftBreak` to insert a line-break instead of splitting nodes
    
    * Modify `packages/slate/src/editor/insert-soft-break.ts` to use `Transforms.insertText` to insert a line-break character instead of splitting nodes
    
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/ianstormtaylor/slate/issues/5671?shareId=fcda97f9-03f1-49f6-8cbc-4fc9f4c95470).

--- packages/slate/src/editor/insert-soft-break.ts
+++ packages/slate/src/editor/insert-soft-break.ts
@@ -2,5 +2,5 @@ import { Transforms } from '../interfaces/transforms'
 import { EditorInterface } from '../interfaces/editor'
 
 export const insertSoftBreak: EditorInterface['insertSoftBreak'] = editor => {
-  Transforms.splitNodes(editor, { always: true })
+  Transforms.insertText(editor, '\n')
 }
